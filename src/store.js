4fccc80e4f12990bbadede33c35afa4efe878a88
Jagadeesh v <vemulajagadeesh28@gmail.com>
2024-01-07 19:03:43 +0530

    redux devtool enabled

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
@@ -7,7 +7,10 @@ const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
